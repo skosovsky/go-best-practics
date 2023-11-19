27ed08aa5c1d94365b50d6b18d5bfba45cc894dd
Sergey Kosovsky <skosovsky@gmail.com>
2023-11-19 16:16:55 +0300

    Fix by finding from linters error

--- lesson-5/duplicates/internal/filecrcalt/filecrcalt_test.go
+++ lesson-5/duplicates/internal/filecrcalt/filecrcalt_test.go
@@ -1,12 +1,13 @@
 package filecrcalt
 
 import (
+	"testing"
+
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func Test_fileMD5(t *testing.T) {
-	var appFs = afero.NewOsFs()
+	appFs := afero.NewOsFs()
 	assert.Equal(t, "889b519803b9de3e338330eb365d0884", FileMD5("/Users/skosovsky/Downloads/Test/Test3/Test4/2.csv", appFs))
 }
--- lesson-5/duplicates/pkg/filecrc/filecrc.go
+++ lesson-5/duplicates/pkg/filecrc/filecrc.go
@@ -1,24 +1,30 @@
-// Рассчитывает контрольную сумму файла
+// Package filecrc рассчитывает контрольную сумму файла
 package filecrc
 
 import (
-	"crypto/md5"
+	"crypto/md5" //nolint:gosec
 	"fmt"
-	"github.com/rs/zerolog/log"
 	"io"
 	"os"
+
+	"github.com/rs/zerolog/log"
 )
 
 func FileMD5(path string) string {
 	log.Debug().Stack().Msg("star func fileMD5")
 
-	h := md5.New()
+	h := md5.New() //nolint:gosec
 	f, err := os.Open(path)
-	defer f.Close()
 	if err != nil {
 		log.Error().Stack().Err(err).Msg("error by open file")
 		return ""
 	}
+	defer func(f *os.File) {
+		err = f.Close()
+		if err != nil {
+			log.Error().Stack().Err(err).Msg("error by open file")
+		}
+	}(f)
 
 	_, err = io.Copy(h, f)
 	if err != nil {
